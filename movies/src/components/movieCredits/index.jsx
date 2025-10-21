9fec6f843cd12d3cd141457a9d064153a6a5a709
Grace <20106694@mail.wit.ie>
2025-10-21 21:03:20 +0100

    Changed the Movie Credit page to include photos of actors, in cards structure. Added movieCreditsCards.jsx to follow movieCard.jsx structure. Actors have a view details button, which have another button to view profile. This button currently links nowhere.

--- movies/src/components/movieCredits/index.jsx
+++ movies/src/components/movieCredits/index.jsx
@@ -1,76 +1,43 @@
-import React, { useState } from "react";
-import Chip from "@mui/material/Chip";
-import Paper from "@mui/material/Paper";
-import AccessTimeIcon from "@mui/icons-material/AccessTime";
-import MonetizationIcon from "@mui/icons-material/MonetizationOn";
-import StarRate from "@mui/icons-material/StarRate";
-import NavigationIcon from "@mui/icons-material/Navigation";
-import Fab from "@mui/material/Fab";
+import React from "react";
 import Typography from "@mui/material/Typography";
-import Drawer from "@mui/material/Drawer";
-import MovieReviews from "../movieReviews"
-import { Link } from "react-router";
-import Button from "@mui/material/Button";
+import Paper from "@mui/material/Paper";
+import Chip from "@mui/material/Chip";
+import MovieCreditsCards from "../movieCreditsCards";
 
 const root = {
-    display: "flex",
-    justifyContent: "center",
-    flexWrap: "wrap",
-    listStyle: "none",
-    padding: 1.5,
-    margin: 0,
+  display: "flex",
+  justifyContent: "center",
+  flexWrap: "wrap",
+  listStyle: "none",
+  padding: 1.5,
+  margin: 0,
 };
 const chip = { margin: 0.5 };
 
-const MovieCredits = ({ credits }) => {  // Don't miss this!
-const [drawerOpen, setDrawerOpen] = useState(false);
-
-const cast = credits.cast
-const names = credits.cast.actors
+const MovieCredits = ({ credits }) => {
+  const cast = credits && Array.isArray(credits.cast) ? credits.cast : [];
 
   return (
-    
-    <> 
+    <>
+      <Typography variant="h5" sx={{ mb: 1 }}>
+        Movie Cast
+      </Typography>
+      <Typography variant="h6" sx={{ mb: 2 }}>
+        This movie features {cast.length} actors.
+      </Typography>
 
-<Typography variant="h5">
-    Movie Cast
-</Typography>
-
-<Typography variant="h6">
-    This movie features {cast.length} actors.
-</Typography>
-
-<Paper component="ul" sx={{...root}} >
-    <li>
-        <Chip label="Cast Members" sx={{...chip}} color="primary" />
-    </li>
-    <li>
-        <Chip label={`${cast.length} total`} sx={{...chip}} />
-    </li>
-</Paper>
-
-<Paper component="ul" sx={{...root}} >
-    <li>
-        <Chip label="Cast Names" sx={{...chip}} color="primary" />
-    </li>
-    <li>
-        <Chip label={`${names}`} />
-    </li>
-</Paper>
-
-
-<Paper component="ul" sx={{...root}}>
-    <li>
-        <Chip label="Cast Names" sx={{...chip}} color="primary" />
-    </li>
-        {cast.map((actor) => (
-          <li key={actor.id}>
-            <Chip label={actor.name} sx={{...chip}} />
-          </li>
-        ))}
+      <Paper sx={{ ...root }}>
+        <li>
+          <Chip label="Cast Members" sx={{ ...chip }} color="primary" />
+        </li>
+        <li>
+          <Chip label={`${cast.length} total`} sx={{ ...chip }} />
+        </li>
       </Paper>
 
-      </>
+      <MovieCreditsCards cast={cast} />
+    </>
   );
 };
-export default MovieCredits ;
+
+export default MovieCredits;
